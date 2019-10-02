b916446071d08495b1ba5c01ef01c19757bfd71e
Jake Bridge <jakebridge2005@gmail.com>
2019-10-02 09:58:39 -0600

    Code puts that it dislikes the world

--- git.rb
+++ git.rb
@@ -1,4 +1,4 @@
 puts "Coding is fun!!!!!"
 puts "Here's some code"
-puts "It prints \"Hello world\""
-puts "Hello world"
+puts "It prints \"Hello world, i dislike you\""
+puts "Hello world, i dislike you"
